22f5bc8813833af3acf824349470e5602dd7fe8e
kenzi <brayen.kenzi@gmail.com>
2020-09-25 14:47:13 +0800

    integrasikan detail travel

--- travelers/routes/web.php
+++ travelers/routes/web.php
@@ -11,7 +11,12 @@
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'HomeController@index')
+->name('home');
+
+Route::get('/detail', 'DetailController@index')
+->name('detail');
+
 
 Route::prefix('admin')
     ->namespace('Admin')
