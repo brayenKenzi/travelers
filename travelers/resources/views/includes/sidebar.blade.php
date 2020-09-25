aaf690f1a31f8ecedd3832ff76ea420244997878
kenzi <brayen.kenzi@gmail.com>
2020-09-25 08:23:05 +0800

    memindahkan html ke laravel

---
+++ travelers/resources/views/includes/sidebar.blade.php
@@ -0,0 +1,59 @@
+<!-- Sidebar -->
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-text mx-3">
+          Travelers Admin
+      </div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item active">
+      <a class="nav-link" href="index.html">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+
+    <hr class="sidebar-divider my-0">
+
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item ">
+            <a class="nav-link" href="index.html">
+              <i class="fas fa-fw fa-hotel"></i>
+              <span>Paket Travel</span></a>
+          </li>
+      
+          <hr class="sidebar-divider my-0">
+
+              <!-- Nav Item - Dashboard -->
+    <li class="nav-item ">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-images"></i>
+          <span>Galerry Travel</span></a>
+      </li>
+  
+      <hr class="sidebar-divider my-0">
+
+          <!-- Nav Item - Dashboard -->
+    <li class="nav-item ">
+        <a class="nav-link" href="index.html">
+          <i class="fas fa-fw fa-dollar-sign"></i>
+          <span>Transaksi</span></a>
+      </li>
+  
+      <hr class="sidebar-divider my-0">
+
+
+
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline mt-3">
+      <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+  </ul>
+  <!-- End of Sidebar -->
