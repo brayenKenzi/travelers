90dc12009e678b35d0cfef3965f35f71b05ee740
kenzi <brayen.kenzi@gmail.com>
2020-09-28 13:20:20 +0800

    menambahkan field username, flow : dari pembuatan field username - ubah form di register.blade - menambahkan validator di RegisterController - menambahkan fillable di User.php

--- travelers/app/User.php
+++ travelers/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail //implementasi Mus
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'username', 'email', 'password', //menambahkan username di field users  
     ];
 
     /**
