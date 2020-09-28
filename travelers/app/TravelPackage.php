a78542270761f00b098024fc5cfa39e1465d1485
kenzi <brayen.kenzi@gmail.com>
2020-09-28 13:31:20 +0800

    menambahkan model travelpackage

---
+++ travelers/app/TravelPackage.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; //agar bisa dipakai soft delete nya maka harus di panggil dlu
+
+
+// untuk membuat model : php artisan make:model TravelPackage
+class TravelPackage extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title', 'slug', 'location', 'about', 'featured_event', 
+        'language', 'foods', 'departure_date', 'duration', 'type', 'price'
+        //field dari table Travel_package
+    ];
+
+    protected $hidden = [
+
+    ];
+}
