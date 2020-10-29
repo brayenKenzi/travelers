abda9338de69d4040b098a8f1be62bde7abe5185
kenzi <brayen.kenzi@gmail.com>
2020-10-29 09:53:30 +0800

    memperbaiki page verify email

--- travelers/resources/views/auth/verify.blade.php
+++ travelers/resources/views/auth/verify.blade.php
@@ -1,28 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.applogin')
 
+@section('title')
+    Verify email
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<main class="verify-container">
+    <div class="container">
+      <div class="d-flex align-items-center">
+        <div class="col-12 text-center mt-4">
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
+        <img src="{{ url('/frontend/images/logo.png')}}" class="logoVerify">
+          <p class="mt-4 registering">Thank you for registering!</p>
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
-            </div>
+                @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('A fresh verification link has been sent to your email address.') }}
+                            </div>
+                @endif
+
+          <img src="{{{ url('/frontend/images/email.png')}}}" class="emailVerify">
+
+          <h3 class="verify">Verify your email address</h3>
+
+          <p class="mb-0">Please click on the link that has just been sent to your email account to verify your email and <br>
+          continue the transaction process.</p>
+
+            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+                @csrf
+                <p class="mb-5 mt-0">haven't you received the email?
+                    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('send another email') }}</button>
+                </p>    
+            </form>
+          
+          <a href="/" class="backHome"><span><img src="{{ url('/frontend/images/back.png')}}" class="mr-2"></span>Back to Homepage</a>
         </div>
+      </div>
     </div>
-</div>
+  </main>
 @endsection
